fe066b723a71369fa70ccdf303e6f42efa09fc96
s9poa <s9poah@gmail.com>
2025-11-06 07:04:39 -0500

    Added Correct images to MainUI footer

--- src/frontend/components/main-ui/footer.tsx
+++ src/frontend/components/main-ui/footer.tsx
@@ -4,11 +4,11 @@ import { Link } from 'react-router';
 function MainUIFooter () {
     return (
         <div className={styles.footer}>
-            <Link to="" aria-label="Street"><img src="/public/icons/street-icon.webp" width="40" height="40" alt="" /></Link>
-            <Link to="" aria-label="Artists"><img src="/public/icons/artists-icon.webp" width="40" height="40" alt="" /></Link>
-            <Link to="" aria-label="Studio"><img src="/public/icons/studio-icon.webp" width="40" height="40" alt="" /></Link>
-            <Link to="" aria-label="Tour"><img src="/public/icons/tour-icon.webp" width="40" height="40" alt="" /></Link>
-            <Link to="" aria-label="Group"><img src="/public/icons/group-icon.webp" width="40" height="40" alt="" /></Link>
+            <Link to="" aria-label="Street"><img src="icons/street-icon.webp" width="40" height="40" alt="" /></Link>
+            <Link to="" aria-label="Artists"><img src="icons/artists-icon.webp" width="40" height="40" alt="" /></Link>
+            <Link to="" aria-label="Studio"><img src="icons/studio-icon.webp" width="40" height="40" alt="" /></Link>
+            <Link to="" aria-label="Tour"><img src="icons/tour-icon.webp" width="40" height="40" alt="" /></Link>
+            <Link to="" aria-label="Group"><img src="icons/group-icon.webp" width="40" height="40" alt="" /></Link>
         </div>
     )
 }
