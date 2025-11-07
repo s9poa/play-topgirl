d3a9e03be466bb1686a703a7d0ba547e7ca8f50a
s9poa <s9poah@gmail.com>
2025-11-07 12:53:14 -0500

    Finalized Design for profile card

--- src/frontend/components/pop-ups/profile.tsx
+++ src/frontend/components/pop-ups/profile.tsx
@@ -9,6 +9,7 @@ const ProfilePopUp = forwardRef<HTMLDivElement, ProfilePopUpProps>(({contentRef}
     return (
     <div ref={blurRef} className={styles.blur}>
         <div ref={contentRef} className={styles.contentContainer}>
+            <div className={styles.contentContainerOverflow}>
             <div className={styles.header}>
                 <div className={styles.infoTab}>
                     <span>Information</span>
@@ -27,14 +28,81 @@ const ProfilePopUp = forwardRef<HTMLDivElement, ProfilePopUpProps>(({contentRef}
                         </div>
                     </div>
                     <div>
-                        <span className={styles.level}>LV.1</span>
+                        <span className={styles.level}>LV.76</span>
                         <div>
                             <span className={styles.name}>username</span>
                             <button className={styles.rename}><img src="/icons/rename-icon.webp" width="40" height="40" alt="" /></button>
                         </div>
                     </div>
                 </div>
-                <div className={styles.gridRow}></div>
+                <div className={styles.gridRow}>
+                    <div>
+                        <div className={styles.tab}>
+                            <span>Max Employees</span>
+                            <img src="/icons/info-icon.webp" width="20" height="20" alt="" />
+                        </div>
+                        <div className={styles.tabContent}>
+                            <span>1,820</span>
+                            <span>Next +30</span>
+                        </div>
+                    </div>
+                    <div>
+                        <div className={styles.tab}>
+                            <span>Available Single Levels</span>
+                            <img src="/icons/info-icon.webp" width="20" height="20" alt="" />
+                        </div>
+                        <div className={styles.tabContent}>
+                            <span>16</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className={styles.mainContainer}>
+                <span className={styles.title}>Capital Strategist 6</span>
+                <div className={styles.griddenPattern}>
+                    <div>
+                        <div className={styles.labelContainer}>
+                            <img src='/icons/total-sales-icon.webp' width="20" height="20" alt="" />
+                            <span>Total Sales</span>
+                        </div>
+                        <div className={styles.progressContainer}>
+                            <div className={styles.progressBar}><span>4,482.76T/5000T</span></div>
+                            <img src='/icons/plus-icon.webp' className={styles.plusIcon} width="30" height="30" alt="" />
+                        </div>
+                    </div>
+                    <div>
+                        <div className={styles.labelContainer}>
+                            <img src='/icons/spark-cash-icon.webp' width="20" height="20" alt="" />
+                            <span>Total Finance</span>
+                        </div>
+                        <div className={styles.progressContainer}>
+                            <div className={styles.pbtf}><span>17.96B/6.40B</span></div>
+                            <img src='/icons/check-icon.webp' width="30" height="30" alt="" />
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.actionContainer}>
+                    <div>
+                        <img src="/icons/daily-news-icon.webp" width="30" height="30" alt="" />
+                        <span>Daily News</span>
+                    </div>
+                    <button>Advance</button>
+                </div>
+            </div>
+            <div className={styles.footer}>
+                <div>
+                    <img src="/icons/rankings-icon.webp" width="40" height="40" alt="" />
+                    <span>Rankings</span>
+                </div>
+                <div>
+                    <img src="/icons/business-card-icon.webp" width="40" height="40" alt="" />
+                    <span>Business Card</span>
+                </div>
+                <div>
+                    <img src="/icons/settings-icon.webp" width="40" height="40" alt="" />
+                    <span>Settings</span>
+                </div>
+            </div>
             </div>
         </div>
     </div>
